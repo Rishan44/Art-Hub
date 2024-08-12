c4a69eca3b21f4d6de84df5b5ccea62bc083da17
Rishan44 <rishanrishu24@gmail.com>
2024-08-12 10:57:29 +0530

    created user model

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,18 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv'
-dotenv.config()
+const express = require('express');
+const userRoutes = require('./Routes/userRoute.js'); 
+const authRoutes = require ('./Routes/authRoute.js');
 
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log('Connected to MongoDb');
-}).catch((err)=>{
-    console.log(err);
-})
-
+const { mongoConnect } = require('./config/db.js')
+require('dotenv').config() 
 const app = express();
+const http = require('http').createServer(app)
+mongoConnect()
 
+
+app.use(express.json())
 app.listen(3000, ()=>{
     console.log('server listening on port 3000');
 })
+
+// app.use('/backend/user',userRoutes)
+// app.use('/backend/auth',authRoutes)
